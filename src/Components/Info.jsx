243258f2a2b019bf51a26826c9f7cb7041c15d18
Erickw22 <erick.weslley.lima@gmail.com>
2025-06-04 02:59:24 -0300

    teste06

--- src/Components/Info.jsx
+++ src/Components/Info.jsx
@@ -3,7 +3,7 @@ import { useParams, useNavigate } from "react-router-dom";
 import axios from "axios";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import "../styles/Info.css";
+import "./info.css";
 
 import ToastService from "../assets/toastService";
 
